1d707133f0d6516a93f895435e997821d40e9066
Kasleets <141558322+Kasleets@users.noreply.github.com>
2023-09-12 13:44:22 +0200

    trying to synchronise

--- GithubTests1/Program.cs
+++ GithubTests1/Program.cs
@@ -6,6 +6,12 @@
         {
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Testing if I copied repo correctly on GitHub");                   //Hello there!
+
+            Console.WriteLine("Testing if I copied repo correctly on GitHub Again!");
+
+            Console.WriteLine("My potato doesn't synchronise with Git!!");
+
+            //weird stuff still can't synchronise
         }
     }
 }
