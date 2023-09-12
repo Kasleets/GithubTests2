3e5fe1b501049fedc5b1d6cbd38e8558ef59cc59
Kasleets <141558322+Kasleets@users.noreply.github.com>
2023-09-12 13:27:30 +0200

    Add project files.

---
+++ GithubTests1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace GithubTests1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Testing if I copied repo correctly on GitHub");                   //Hello there!
+        }
+    }
+}
